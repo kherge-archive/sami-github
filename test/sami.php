90dc833f701c505ad4c8db91ab935fca4dbea05e
Kevin Herrera <kevin@herrera.io>
2013-09-11 08:38:05 -0700

    Rendering public and protected members.

--- test/sami.php
+++ test/sami.php
@@ -1,9 +1,13 @@
 <?php
 
+use Sami\Parser\Filter\FilterInterface;
+use Sami\Reflection\ClassReflection;
+use Sami\Reflection\MethodReflection;
+use Sami\Reflection\PropertyReflection;
 use Sami\Sami;
 use Symfony\Component\Finder\Finder;
 
-return new Sami(
+$sami = new Sami(
     Finder::create()
         ->files()
         ->name('*.php')
@@ -15,3 +19,34 @@ return new Sami(
         'theme' => 'github',
     )
 );
+
+$sami['filter'] = $sami->share(
+    function () {
+        return new CustomFilter();
+    }
+);
+
+/**
+ * Allows public and protected members to be rendered.
+ *
+ * @author Kevin Herrera <kevin@herrera.io>
+ */
+class CustomFilter implements FilterInterface
+{
+    public function acceptClass(ClassReflection $class)
+    {
+        return true;
+    }
+
+    public function acceptMethod(MethodReflection $method)
+    {
+        return ($method->isPublic() || $method->isProtected());
+    }
+
+    public function acceptProperty(PropertyReflection $property)
+    {
+        return ($property->isPublic() || $property->isProtected());
+    }
+}
+
+return $sami;
